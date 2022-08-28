56001e5e67309ac50cd2ed08d3fc1495347ff3bd
sajonaro <alex.kunich@gmail.com>
2022-08-28 14:07:17 +0300

    added s3 functions

--- src/s3.clj
+++ src/s3.clj
@@ -1,1 +1,20 @@
-(ns s3)
+(ns s3
+  (:require [permutations :as p]
+            [printing :as prnt]
+            [clojure.pprint :as pp]
+            [clojure.java.io :as io]))
+
+
+
+  (def s3members
+    (merge
+     {:e '(1 2 3)} ;;; e*g=g*e, for any g in S3
+     (zipmap
+      '(g1 g2 g3 g4 g5)
+      (next (p/gen-permutations '(1 2 3))))))
+
+(pp/pprint s3members (io/writer "output/s3.txt"))
+
+  
+  
+(prnt/print-multiplication-table s3members p/multiply-permutations)
